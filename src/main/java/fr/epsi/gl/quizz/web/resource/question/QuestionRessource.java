eb6e3f5250fdc476eba1b0b10b40d45aeabdfc10
jb <bodysplash@gmail.com>
2013-04-13 11:30:17 +0200

    commande ajout réponse
    formulaire ajout de réponse

--- src/main/java/fr/epsi/gl/quizz/web/resource/question/QuestionRessource.java
+++ src/main/java/fr/epsi/gl/quizz/web/resource/question/QuestionRessource.java
@@ -1,10 +1,15 @@
 package fr.epsi.gl.quizz.web.resource.question;
 
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
+import fr.epsi.gl.quizz.commande.BusCommande;
+import fr.epsi.gl.quizz.commande.question.AjoutReponseMessage;
 import fr.epsi.gl.quizz.requete.question.DetailsQuestion;
 import fr.epsi.gl.quizz.requete.question.RechercheQuestions;
 import fr.epsi.gl.quizz.web.representation.ModeleEtVue;
+import org.restlet.data.Form;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
@@ -14,8 +19,9 @@ public class QuestionRessource extends ServerResource
 {
 
     @Inject
-    public QuestionRessource(RechercheQuestions recherche) {
+    public QuestionRessource(RechercheQuestions recherche, BusCommande bus) {
         this.recherche = recherche;
+        this.bus = bus;
     }
 
     @Override
@@ -29,6 +35,14 @@ public class QuestionRessource extends ServerResource
         return ModeleEtVue.crée("/question/question").avec("question", question);
     }
 
+    @Put
+    public void ajouteRéponse(Form formulaire) {
+        boolean correcte = !Strings.isNullOrEmpty(formulaire.getFirstValue("correcte"));
+        AjoutReponseMessage message = new AjoutReponseMessage(UUID.fromString(question.getId()), formulaire.getFirstValue("libelle"), correcte);
+        bus.envoie(message);
+    }
+
     private DetailsQuestion question;
     private RechercheQuestions recherche;
+    private BusCommande bus;
 }
