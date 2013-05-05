06a665e324d0a63f610eb3f22a671df68c07c7c8
Dowmed <info.aroche@gmail.com>
2013-05-05 17:19:26 +0200

    Ajout de la suppression

--- src/main/java/fr/epsi/gl/quizz/commande/question/SuppressionQuestionHandler.java
+++ src/main/java/fr/epsi/gl/quizz/commande/question/SuppressionQuestionHandler.java
@@ -15,12 +15,16 @@ import fr.epsi.gl.quizz.domaine.question.Question;
 public class SuppressionQuestionHandler implements HandlerCommande<SuppressionQuestionMessage> {
 
     public Boolean execute(SuppressionQuestionMessage commande){
-        if(Entrepots.questions().get(commande.id).isPresent()){
+        if(PeutTrouverLaQuestion(commande)){
             Optional<Question> questionASupprimer = Entrepots.questions().get(commande.id);
             Entrepots.questions().supprime(questionASupprimer.get());
             return true;
         }
         return false;
+    }
+
+    private boolean PeutTrouverLaQuestion(SuppressionQuestionMessage commande) {
+        return Entrepots.questions().get(commande.id).isPresent();
     }
 
     public Class<SuppressionQuestionMessage> typeCommande() {
